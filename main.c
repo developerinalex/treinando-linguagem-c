24e6c4ce6fb1408089c6a750d431c5d49441e3e7
allinux <alextrusting1@gmail.com>
2025-03-21 09:14:40 -0300

    problema no if do case 5

---
+++ main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+#include <stdlib.h>
+
+struct pessoa{
+    char nome[50];
+    int idade;
+};
+
+typedef struct pessoa pessoa;
+
+int main(int argc, char *argv[]){
+    setlocale(LC_ALL, "Portuguese");
+
+    pessoa p;
+
+    printf("Olá, seja bem-vindo! Digite o seu nome: ");
+    scanf("%49[^\n]", p.nome);
+    printf("Qual a sua idade? ");
+    scanf("%d", &p.idade);
+
+    system("clear");
+
+    int vet;
+    char ec[4];
+
+    printf("Selecione a dificuldade de 1 a 5: ");
+    scanf("%d", &vet);
+
+    switch(vet){
+        case 1:
+            printf("Pacífico selecionado.\n");
+            break;
+        case 2:
+            printf("Modo fácil selecionado.\n");
+            break;
+        case 3: 
+            printf("Dificuldade média escolhida.\n");
+            break;
+        case 4: 
+            printf("Díficil selecionado");
+            break;
+        case 5: 
+            printf("Insano solicitado. Tem certeza disso? ");
+            scanf("%3s", ec);
+            if(ec == "y"){
+                printf("Insano selecionado. Boa sorte, você irá precisar.\n");
+            } else {
+                printf("failed.\n");
+            }
+            break;
+    }
+    
+
+
+
+}
